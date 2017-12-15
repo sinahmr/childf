0c75b68b6f1e951db177e43dd532e2b3accb2d7f
Sina Hajimiri <sina.hajimiri@gmail.com>
2017-12-15 14:51:56 +0330

    Child edit page is added

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^home/', views.home),
-    url(r'^volunteer/', views.volunteer),
+    url(r'^volunteer/$', views.volunteer),
     url(r'^child-information/', views.child_information),
-    url(r'^add/', views.add_child),
-    url(r'^child/letter/', views.letter),
     url(r'^login/', views.login),
-    url(r'^profile/', views.profile),
+    url(r'^profile/$', views.profile),
+    url(r'^child/add/', views.add_child),
+    url(r'^child/edit/', views.edit_child),
+    url(r'^child/letter/', views.letter),
     url(r'^child/request/', views.send_request),
     url(r'^donor/purchase/', views.purchase),
 ]
