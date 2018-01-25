8db4d0f4f3a47b51b46b7b8e28494e60500303f5
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2018-01-25 13:07:43 +0330

    Fix saving and loading images

--- childf/urls.py
+++ childf/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
